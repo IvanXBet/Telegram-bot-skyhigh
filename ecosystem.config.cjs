bbe99eaaf950517eaf74ec45bdab1817a2b21776
ivan <ivan24072001@gmail.com>
2024-05-27 10:03:33 +0300

    fix

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,13 +1,15 @@
-export const apps = [
-	{
-		name: 'telegram-bot-skyhigh',
-		script: './index.js',
-		node_args: '--experimental-modules', // Убедитесь, что этот флаг установлен
-		env: {
-			NODE_ENV: 'development',
+module.exports = {
+	apps: [
+		{
+			name: 'telegram-bot-skyhigh',
+			script: './index.js',
+			node_args: '--experimental-modules', // добавим эту опцию
+			env: {
+				NODE_ENV: 'development',
+			},
+			env_production: {
+				NODE_ENV: 'production',
+			},
 		},
-		env_production: {
-			NODE_ENV: 'production',
-		},
-	},
-]
+	],
+}
