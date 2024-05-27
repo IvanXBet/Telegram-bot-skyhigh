c9faac649081384b0a0e5579b5ffcafc953d8d6f
IvanXBet <ivan24072001@gmail.com>
2024-05-28 00:09:31 +0300

    add controllers

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import TelegramBot from 'node-telegram-bot-api'
 import { checkStreamStatus } from './twitch-api/wtitch-api.mjs'
+
+import { messageListener } from './controllers/messageController.js'
 import 'dotenv/config.js'
 
 
@@ -16,58 +18,12 @@ export async function botStart() {
 	await messageListener()
 }
 
-export async function messageListener() {
-	bot.on('message', async msg => {
-		const text = msg.text
-		const chatID = msg.chat.id
 
-		try {
-			switch (text) {
-				case '/start':
-					return bot.sendMessage(chatID, `👋🏻`)
-
-				case '/info':
-					return await bot.sendMessage(
-						chatID,
-						`Этого бота я создал в образовательных целях.\nУ него много функций, одна из них это уведомлять о начале моих стримов.\nСтримы тут: <a href='https://www.twitch.tv/skyhightm'>Ссылка на мой Twitch канал</a>`,
-						{
-							parse_mode: 'HTML',
-							disable_web_page_preview: true,
-						}
-					)
-
-				case '/anons':
-					return await sendTelegramMessage(
-						chatID,
-						`Ты подписался на анонсы стримов.\nКогда начнется трансляция, я тебе пришлю уведомление.`
-					)
-
-				case '/admin':
-					if (+chatID == process.env.ADMIN_ID) {
-						const status = await checkStreamStatus()
-
-						if ((status.status = 1)) {
-							return await sendTelegramMessage(chatID, status.live)
-						} else {
-							return console.log(status.error)
-						}
-					} else {
-						return await sendTelegramMessage(chatID, 'ТЫ НЕ АДМИН')
-						break
-					}
-
-				default:
-					return await sendTelegramMessage(chatID, 'Я тебя не понимаю')
-					break
-			}
-		} catch (error) {
-			await sendTelegramMessage(chatID, `${error}`)
-		}
-	})
-}
 
 export async function sendTelegramMessage(chatID, message) {
 	return bot.sendMessage(chatID, message)
 }
 
 botStart();
+
+console.log('Bot is running...');
